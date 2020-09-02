f56451f53389363bc58e01d41fabf1742cf26ee1
cichyvx <filipcichyf14@gmail.com>
2020-09-02 18:42:59 +0200

    added method returning Title by name

--- src/main/java/map/ground/Title.java
+++ src/main/java/map/ground/Title.java
@@ -4,6 +4,14 @@ import java.awt.*;
 
 public interface Title {
 
+    static Title getTitle(String name, int x, int y){
+        if(name.equals(new DefaultTitle(0,0).toString())) return new DefaultTitle(x, y);
+        if(name.equals(new GrassTitle(0,0).toString())) return new GrassTitle(x, y);
+        if(name.equals(new RockTitle(0,0).toString())) return new RockTitle(x, y);
+        if(name.equals(new WaterTitle(0,0).toString())) return new WaterTitle(x, y);
+        return null;
+    }
+
     public int getId();
     public int getX();
     public int getY();
