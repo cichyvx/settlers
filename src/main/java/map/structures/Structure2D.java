f41183b220b7b5a9da94f79031a435e41bb24800
cichyvx <filipcichyf14@gmail.com>
2020-09-15 11:29:56 +0200

    added enum with structure id

--- src/main/java/map/structures/Structure2D.java
+++ src/main/java/map/structures/Structure2D.java
@@ -1,5 +1,19 @@
 package map.structures;
 
 public interface Structure2D {
+    int TREE = 1;
 
+    static int getStructuresCount(){
+        return 1;
+    }
+
+    static Structure2D getStructure(String name){
+        if(name.equals(new TreeStructure().toString())) return new TreeStructure();
+        return null;
+    }
+
+    static Structure2D getStructure(int id){
+        if(id == TREE) return new TreeStructure();
+        return null;
+    }
 }
