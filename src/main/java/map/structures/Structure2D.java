d936e7dad181cbae2e4dfa67460bbdaa1f2471ce
cichyvx <filipcichyf14@gmail.com>
2020-09-16 23:23:07 +0200

    added PlantStructure to getting method

--- src/main/java/map/structures/Structure2D.java
+++ src/main/java/map/structures/Structure2D.java
@@ -2,6 +2,7 @@ package map.structures;
 
 public interface Structure2D {
     int TREE = 1;
+    int PLANT = 2;
 
     static int getStructuresCount(){
         return 1;
@@ -9,11 +10,13 @@ public interface Structure2D {
 
     static Structure2D getStructure(String name){
         if(name.equals(new TreeStructure().toString())) return new TreeStructure();
+        if(name.equals(new PlantStructure().toString())) return new PlantStructure();
         return null;
     }
 
     static Structure2D getStructure(int id){
         if(id == TREE) return new TreeStructure();
+        if(id == PLANT) return new PlantStructure();
         return null;
     }
 }
