9a0b1a84bcc22412907b27b3ddc3dccc69204266
cichyvx <filipcichyf14@gmail.com>
2020-10-31 20:20:09 +0100

    initial commit interface for animal objects

---
+++ src/main/java/creatures/animals/Animal.java
@@ -0,0 +1,29 @@
+package creatures.animals;
+
+public interface Animal {
+    static Animal getAnimal(int selected) {
+        switch (selected){
+            case 1:
+                return new Cow(0,0,0,0);
+            default:
+                return new Cow(0,0,0,0);
+        }
+    }
+
+    double getX();
+    double getY();
+    int getWidth();
+    int getHeight();
+
+    void setX(double x);
+    void setY(double y);
+    void setWeight(int weight);
+    void setHeight(int height);
+
+    void update();
+
+    static int getAnimalsCount(){
+        return 1;
+    }
+
+}
