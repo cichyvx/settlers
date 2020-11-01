15b96beaf9b4caddee54f4f21c514e7d7657ca4e
cichyvx <filipcichyf14@gmail.com>
2020-11-01 13:43:40 +0100

    added animal converter

--- src/main/java/conventers/TitleHolder.java
+++ src/main/java/conventers/TitleHolder.java
@@ -6,7 +6,8 @@ import java.util.List;
 public class TitleHolder {
     public final String name, autor, description;
     public final int width, height;
-    private List<TitleConventer> list;
+    private List<TitleConventer> titleList;
+    private List<AnimalConventer> animallist;
 
     public TitleHolder(int width, int height, String name, String autor, String description){
         this.name = name;
@@ -14,15 +15,20 @@ public class TitleHolder {
         this.description = description;
         this.width = width;
         this.height = height;
-        list  = new ArrayList<>();
+        titleList  = new ArrayList<>();
+        animallist = new ArrayList<>();
     }
 
-    public void add(TitleConventer titleConventer){
-        list.add(titleConventer);
+    public void addTitle(TitleConventer titleConventer){
+        titleList.add(titleConventer);
     }
 
-    public List<TitleConventer> getList(){
-        return list;
+    public void addAnimal(AnimalConventer animalConventer) { animallist.add(animalConventer); }
+
+    public List<TitleConventer> getTitles(){
+        return titleList;
     }
+
+    public List<AnimalConventer> getAnimals() { return animallist; }
 
 }
