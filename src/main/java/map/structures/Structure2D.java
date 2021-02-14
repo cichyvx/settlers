10829df0ec90a279a9ddb6d0c15479733e42fe1f
cichyvx <filipcichyf14@gmail.com>
2021-02-14 16:01:49 +0100

    added Barres class

--- src/main/java/map/structures/Structure2D.java
+++ src/main/java/map/structures/Structure2D.java
@@ -5,20 +5,23 @@ import debuger.SettlerDebuger;
 public interface Structure2D extends SettlerDebuger {
     int TREE = 1;
     int PLANT = 2;
+    int BERRIES = 3;
 
     static int getStructuresCount(){
-        return 2;
+        return 3;
     }
 
     static Structure2D getStructure(String name){
         if(name.equals(new TreeStructure().toString())) return new TreeStructure();
         if(name.equals(new PlantStructure().toString())) return new PlantStructure();
+        if(name.equals(new BerriesStructure().toString())) return new BerriesStructure();
         return null;
     }
 
     static Structure2D getStructure(int id){
         if(id == TREE) return new TreeStructure();
         if(id == PLANT) return new PlantStructure();
+        if(id == BERRIES) return new BerriesStructure();
         return null;
     }
 }
