c06ddbbb3e2f8861a332412bc7ad0ef8af0b5743
cichyvx <filipcichyf14@gmail.com>
2021-02-17 22:17:58 +0100

    documentation was written

--- src/main/java/conventers/TitleConventer.java
+++ src/main/java/conventers/TitleConventer.java
@@ -3,10 +3,28 @@ package conventers;
 import map.ground.Title;
 import map.structures.Structure2D;
 
+/**
+ * the class is used to convert Titles to one typo in order to save them,
+ * among others, to a JSON file, or to convert them from the current class
+ * to the state before conversion
+ */
+
+
 public class TitleConventer {
     private int x,y;
     private String name, structure;
 
+    /**
+     * turns any animal into a safe write-ready type
+     * @param x horizontal position of the field
+     * @param y vertical position of the field
+     * @param name name of the field
+     * @param WIDTH width of the field
+     * @param HEIGHT height of the field
+     * @param autor author of the entire map
+     * @param description description of the entire map
+     * @param structure the structure in the field
+     */
     public TitleConventer(int x, int y, String name, int WIDTH, int HEIGHT, String autor, String description, String structure){
         this.x = x;
         this.y = y;
@@ -14,6 +32,10 @@ public class TitleConventer {
         this.structure = structure;
     }
 
+    /**
+     * turns any animal into a safe write-ready type
+     * @param title to be converted
+     */
     public TitleConventer(Title title){
         this.x = title.getX();
         this.y = title.getY();
@@ -27,6 +49,12 @@ public class TitleConventer {
     public TitleConventer(){
 
     }
+
+    /**
+     *
+     * @param titleConventer safe type to be converted
+     * @return original type before converting
+     */
 
     public static Title getTitle(TitleConventer titleConventer){
         if(titleConventer.getStructure() == null)
