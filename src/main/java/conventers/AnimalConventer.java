8e99376bd77bf73dc4691696c56b9bb56ea7e5b0
cichyvx <filipcichyf14@gmail.com>
2021-02-17 22:12:25 +0100

    documentation was written

--- src/main/java/conventers/AnimalConventer.java
+++ src/main/java/conventers/AnimalConventer.java
@@ -2,12 +2,22 @@ package conventers;
 
 import creatures.animals.Animal;
 
+/**
+ * the class is used to convert animals to one typo in order to save them,
+ * among others, to a JSON file, or to convert them from the current class
+ * to the state before conversion
+ */
+
 public class AnimalConventer {
 
     private double x, y;
     private int width, height;
     private String name;
 
+    /**
+     * turns any animal into a safe write-ready type
+     * @param animal animal to be converted
+     */
     public AnimalConventer(Animal animal){
         this.x = animal.getX();
         this.y = animal.getY();
@@ -17,6 +27,11 @@ public class AnimalConventer {
     }
 
 
+    /**
+     *
+     * @param animalConventer safe type to be converted
+     * @return original type before converting
+     */
     public static Animal AgetAnimal(AnimalConventer animalConventer){
         Animal animal = Animal.getAnimal(animalConventer.name);
         animal.setX(animalConventer.x);
